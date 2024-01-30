73927dd17cd287609a752a8450a780ee40a0ae65
Berlandro <bryangodlive0@gmail.com>
2024-01-30 10:30:45 +0700

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'swiper/css';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './pages/home.jsx';
-import LoginPage from './pages/loginPage.jsx';
-import RegisterPage from './pages/registerPage.jsx';
+import Home from './pages/homePage';
+import LoginPage from './pages/loginPage';
+import RegisterPage from './pages/registerPage';
 
 // const router = createBrowserRouter([
 //   {
