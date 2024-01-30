2da4f5deff2c2d18bd7bfa8b5e2eba36af9eded6
Berlandro <bryangodlive0@gmail.com>
2024-01-30 09:59:04 +0700

    change RRD version

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'swiper/css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes} from 'react-router-dom';
 import { Home } from '../src/pages/home.jsx';
 import LoginPage from '../src/pages/loginPage.jsx';
 import RegisterPage from './pages/registerPage.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home/>
-  },
-  {
-    path: '/login',
-    element: <LoginPage/>
-  },
-  {
-    path: '/register',
-    element: <RegisterPage/>
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Home/>
+//   },
+//   {
+//     path: '/login',
+//     element: <LoginPage/>
+//   },
+//   {
+//     path: '/register',
+//     element: <RegisterPage/>
+//   }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    {/* <RouterProvider router={router}/> */}
+    <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
